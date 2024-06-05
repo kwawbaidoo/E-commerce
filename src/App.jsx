cb78afab29529f4b27eda132188634c2f4f33eaf
kwawbaidoo <kwaw.baidoo@gmail.com>
2024-06-05 11:12:02 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+import Top from '../components/Top';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    hello
+    <Top/>
+    </>
+  )
+}
+
+export default App
