65d155a5dec1dbd6ec26a49aea1f310f83bd989c
kwawbaidoo <kwaw.baidoo@gmail.com>
2024-06-06 11:53:31 +0000

    flash sales component

--- src/components/Flashsales.jsx
+++ src/components/Flashsales.jsx
@@ -2,7 +2,7 @@ const Flashsales = () => {
   return (
     <div className="flex justify-center items-center border border-red-300 mt-36">
       <section className="flex flex-col items- justify-center w-full xl:max-w-[1170px] md:max-w-[1170px] border">
-        <div className="flex  xl:max-w-[600px] md:max-w-[600px] border">
+        <div className="flex  xl:max-w-[600px] md:max-w-[600px] justify-between border">
           <div className="flex flex-col gap-6">
             <span className="flex items-center gap-4">
               <span className="w-5 h-10 bg-customred rounded-md "></span>
@@ -13,28 +13,26 @@ const Flashsales = () => {
             <h3 className="font-inter font-semibold text-4xl">Flash Sales</h3>
           </div>
 
-          <div className="flex ">
-            <span className="flex flex-col">
-              <h3>Days</h3>
-              <h3>03</h3>
-              <h3>:</h3>
+          <div className="flex items-end justify-between gap- border">
+            <span className="flex flex-col items- gap-1 mr-4 border">
+              <h3 className="text-xs font-poppins font-medium">Days</h3>
+              <h3 className="text-3xl font-inter font-bold">03</h3>
             </span>
-           
-            <span className="flex flex-col">
-              <h3>Hours</h3>
-              <h3>23</h3>
-              <h3>:</h3>
+            <h3 className="text-5xl text-custometimecolor mr-4 mb-1">:</h3>
+            <span className="flex flex-col items-  gap-1 mr-4 border">
+              <h3 className="text-xs font-poppins font-medium">Hours</h3>
+              <h3 className="text-3xl font-inter font-bold">23</h3>
+             
             </span>
-            
-            <span className="flex flex-col">
-              <h3>Minutes</h3>
-              <h3>19</h3>
-              <h3>:</h3>
+            <h3 className=" text-5xl text-custometimecolor mr-4 mb-1">:</h3>
+            <span className="flex flex-col items-star justify-between gap-1 mr-4 border">
+              <h3 className="text-xs font-poppins font-medium">Minutes</h3>
+              <h3 className="text-3xl font-inter font-bold">19</h3>
             </span>
-            
-            <span className="flex flex-col">
-              <h3>Seconds</h3>
-              <h3>56</h3>
+            <h3 className="text-5xl text-custometimecolor mr-4 mb-1">:</h3>
+            <span className="flex flex-col items- gap-1 border">
+              <h3 className="text-xs font-poppins font-medium">Seconds</h3>
+              <h3 className="text-3xl font-inter font-bold">56</h3>
             </span>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ export default {
       colors:{
         customgray:'#F5F5F5',
         customred: '#DB4444',
+        custometimecolor: '#E07575',
       },
       fontFamily:{
         poppins:['Poppins'],
