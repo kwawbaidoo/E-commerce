7e885ac93f44070fceb3dcc466d9affe8375bfd3
kwawbaidoo <kwaw.baidoo@gmail.com>
2024-06-18 13:25:15 +0000

    working on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,24 +6,44 @@ import shop from "../assets/icons/shop.png";
 import Sale from "../assets/icons/Sale.png";
 import Shoppingbag from "../assets/icons/Shopping bag.png";
 import moneybag from "../assets/icons/Moneybag.png";
-import richmond from "../assets/images/Richmond.jpg";
+import richmond from "../assets/images/Richmond.png";
+import headphone from "../assets/images/Headphone.png";
 
 const About = () => {
   const data = [
-    { icon:  shop , number: 5, details: "Sallers active on our site" },
-    {icon: Sale, number: 33, details: "Monthly monthly Sale"},
+    { icon: shop, number: 5, details: "Sallers active on our site" },
+    { icon: Sale, number: 33, details: "Monthly monthly Sale" },
     { icon: Shoppingbag, number: 5, details: "Customers active on our site" },
     { icon: moneybag, number: 5, details: "Anual gross sale in our site" },
   ];
 
-  const team =[
-    {image : richmond, name: "Richmond Forkuo Afram", role:"Product Designer",linkOne:"link1", linkTwo:"link2", linkThree:"link3"},
-    {image : kwawImage, name: "Francis Baidoo", role:"Managing Director",linkOne:"link1", linkTwo:"link2", linkThree:"link3"}
+  const team = [
+    {
+      image: richmond,
+      name: "Richmond Forkuo Afram",
+      role: "Product Designer",
+      iconOne: "bxl-twitter",
+      iconTwo: "bxl-instagram-alt",
+      iconThree: "bxl-linkedin-square",
+      linkOne: "link1",
+      linkTwo: "link2",
+      linkThree: "link3",
+    },
+    {
+      image: kwawImage,
+      name: "Francis Baidoo",
+      role: "Managing Director",
+      iconOne: "bxl-twitter",
+      iconTwo: "bxl-instagram-alt",
+      iconThree: "bxl-linkedin-square",
+      linkOne: "link1",
+      linkTwo: "link2",
+      linkThree: "link3",
+    },
+  ];
 
-  ]
-
-//   <i class='bx bx-dollar'></i>
-//   <i class='bx bx-store-alt' ></i>
+  //   <i class='bx bx-dollar'></i>
+  //   <i class='bx bx-store-alt' ></i>
   console.log(data);
 
   const location = useLocation();
@@ -64,54 +84,104 @@ const About = () => {
       </div>
 
       <div className="w-full flex items-center justify-center mt-36">
-      <div className="w-full xl:max-w-[1170px] md:max-[1170px] lg:max-w-[1170px] flex gap-8 ">
-            
-            {data.map((item, key)=>(
-
-            <div key={key} className="border w-[270px] h-[230px] rounded-[4px] flex items-center justify-center hover:bg-customred hover:text-white">
-                <div className="flex flex-col gap-6 items-center justify-center">
+        <div className="w-full xl:max-w-[1170px] md:max-[1170px] lg:max-w-[1170px] flex gap-8 ">
+          {data.map((item, key) => (
+            <div
+              key={key}
+              className="border w-[270px] h-[230px] rounded-[4px] flex items-center justify-center hover:bg-customred hover:text-white"
+            >
+              <div className="flex flex-col gap-6 items-center justify-center">
                 <span className="w-20 h-20 rounded-full bg-gray-300 flex items-center justify-center">
-                    <span className="flex w-14 h-14 rounded-full bg-black hover:bg-white items-center justify-center">
-                            <img src={item.icon} alt="" />
-                    </span>
+                  <span className="flex w-14 h-14 rounded-full bg-black hover:bg-white items-center justify-center">
+                    <img src={item.icon} alt="" />
+                  </span>
                 </span>
 
                 <div className="flex flex-col gap-3 items-center justify-center">
-                    <span>{item.number}<span>k</span></span>
-                    <h5>{item.details}</h5>
+                  <span>
+                    {item.number}
+                    <span>k</span>
+                  </span>
+                  <h5>{item.details}</h5>
                 </div>
-                </div>
-
+              </div>
             </div>
-            ))}
-      </div>
-
-     
-
-      </div>
-      <div className="flex mt-36 gap-20 w-full items-center justify-center border">
-        <div className="flex flex-col w-[236px] h-[564px] border">
-
-                <div className="w-[236px] h-[321px] bg-teambg rounded-lg shadow-lg">
-                <img className="w-[236px] h-[321px]" src={kwawImage} alt="" />
-                </div>
-        <span className="flex flex-col gap-4">
-            <span className="flex flex-col gap-2">
-            <h3>name</h3>
-            <h3>Role</h3>
-            </span>
-            <span className="flex gap-4">
-                <a href=""><img src="" alt="" /></a>
-                <a href=""><img src="" alt="" /></a>
-                <a href=""><img src="" alt="" /></a>
-            </span>
-        </span>
-            
+          ))}
         </div>
-
-        
-       
       </div>
+      <div className="flex mt-36 gap-20 w-full items-center justify-center ">
+        {team.map((item, key) => (
+          <div key={key} className="flex flex-col w-[236px] rounded-md ">
+            <div className="w-[236px] h-[321px] bg-teambg rounded-lg shadow-lg">
+              <img className="w-[236px] h-[321px]" src={item.image} alt="" />
+            </div>
+            <span className="flex flex-col gap-4 mt-8">
+              <span className="flex flex-col gap-2">
+                <h3>{item.name}</h3>
+                <h3>{item.role}</h3>
+              </span>
+              <span className="flex gap-4 ">
+                <a
+                  className="w-8 h-8 flex items-center justify-center rounded-full bg-white"
+                  href={item.linkOne}
+                >
+                  <i class={`bx ${item.iconOne} text-2xl`}></i>
+                </a>
+                <a
+                  className="w-8 h-8 flex items-center justify-center rounded-full bg-white"
+                  href={item.linkTwo}
+                >
+                  <i class={`bx ${item.iconTwo} text-2xl`}></i>
+                </a>
+                <a
+                  className="w-8 h-8 flex items-center justify-center rounded-full bg-white"
+                  href={item.linkThree}
+                >
+                  <i class={`bx ${item.iconThree} text-2xl`}></i>
+                </a>
+              </span>
+            </span>
+          </div>
+        ))}
+      </div>
+      <section className="mt-32 flex items-center justify-center">
+        <div className="flex gap-[88px]  w-[943px] ">
+            <div className="flex items-center justify-center flex-col gap-6 ">
+                <span className="w-20 h-20 rounded-full bg-gray-300 flex items-center justify-center">
+                    <span className=" w-16 h-16 rounded-full bg-customblack flex items-center justify-center">
+                        <img className="w-10 h-10 rounded-full" src={iconDelivery} alt="" />
+                    </span>
+                </span>
+                <div className="flex flex-col gap-2 items-center justify-center">
+                    <h3 className="font-poppins font-semibold text-xl">FREE AND FAST DELIVERY</h3>
+                    <h3 className="font-poppins font-normal text-sm">Free delivery for all orders over $140</h3>
+                </div>
+            </div>
+            <div className="flex items-center justify-center flex-col gap-6">
+                <span className="w-20 h-20 rounded-full bg-gray-300 flex items-center justify-center">
+                    <span className=" w-16 h-16 rounded-full bg-customblack flex items-center justify-center">
+                        <img className="w-10 h-10 rounded-full" src={iconCustomerService} alt="" />
+                    </span>
+                </span>
+                <div className="flex flex-col gap-2 items-center justify-center">
+                    <h3 className="font-poppins font-semibold text-xl">24/7 CUSTOMER SERVICE</h3>
+                    <h3 className="font-poppins font-normal text-sm">Friendly 24/7 customer support</h3>
+                </div>
+            </div>
+            <div className="flex items-center justify-center flex-col gap-6 ">
+                <span className="w-20 h-20 rounded-full bg-gray-300 flex items-center justify-center">
+                    <span className=" w-16 h-16 rounded-full bg-customblack flex items-center justify-center">
+                        <img className="w-10 h-10 rounded-full" src={iconSecure} alt="" />
+                    </span>
+                </span>
+                <div className="flex flex-col items-center justify-center gap-2">
+                    <h3 className="font-poppins font-semibold text-xl">MONEY BACK GUARANTEE</h3>
+                    <h3 className="font-poppins font-normal text-sm">We reurn money within 30 days</h3>
+                </div>
+            </div>
+
+        </div>
+      </section>
     </div>
   );
 };
